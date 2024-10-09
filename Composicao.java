338ac1fd96e99cca4b883c7998811786f07dd9f6
fleiper <estudoinglesisso@gmailcom>
2024-10-09 16:35:30 -0300

    composição

---
+++ Composicao.java
@@ -0,0 +1,10 @@
+package Composição;
+
+public class Composicao {
+
+	public static void main(String[] args) {
+		Pessoa pes = new Pessoa ("joão",80);
+		System.out.println(pes.getNome()+ "POSSUI UMA MEDIA DE "+pes.getCoracao()+" batimentos por minuto");
+	}
+
+}
---
+++ Coracao.java
@@ -0,0 +1,16 @@
+package Composição;
+
+public class Coracao {
+	
+	private int Batimento;
+	
+	public Coracao (int Batimento) {
+		this.Batimento = Batimento;
+	}
+	public void setBatimento (int Batimento) {
+		this.Batimento = Batimento;
+	}
+	public int getBatimento() {
+		return Batimento;
+	}
+}
